3a16221ded1dd132d90333968655d15f7cb8fa8c
Gil Pasi <gilpasi@Gils-MacBook-Pro.local>
2023-08-17 08:57:59 +0300

    Second assignment of OS course was added

---
+++ OS-Assignment-2/src/Runner.java
@@ -0,0 +1,40 @@
+/*Authors:
+ * Gil Pasi 	- 206500936
+ * Heba Abu-Kaf - 323980441
+ * */
+public class Runner {
+
+	public static void main(String[] args) {
+		CowThread c1 = new CowThread();
+		CowThread c2 = new CowThread();
+		CowThread c3 = new CowThread();
+		CowThread c4 = new CowThread();
+		CowThread c5 = new CowThread();
+		CowThread c6 = new CowThread();
+		CowThread c7 = new CowThread();
+		CowThread c8 = new CowThread();
+		CowThread c9 = new CowThread();
+		CowThread c10 = new CowThread();
+		CowThread c11 = new CowThread();
+		CowThread c12 = new CowThread();
+
+		c1.start();
+		c2.start();
+		c3.start();
+		c4.start();
+		c5.start();
+		c6.start();
+		c7.start();
+		c8.start();
+		c9.start();
+		c10.start();
+		c11.start();
+		c12.start();
+
+
+
+
+
+	}
+
+}
